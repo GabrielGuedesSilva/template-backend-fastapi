c1e6f1f31e99a2191b2947921972751ec08f0eb3
Gabriel Guedes <gabrielguedesbsilva@gmail.com>
2025-05-04 19:52:36 -0300

    feat: Adding custom types

---
+++ src/core/schemas/types.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+
+from pydantic import Field, StringConstraints
+
+NonEmptyStr = Annotated[
+    str, StringConstraints(min_length=1, strip_whitespace=True)
+]
+NonNegativeInt = Annotated[int, Field(ge=0)]
+NonNegativeFloat = Annotated[float, Field(ge=0)]
